5dd1cd3188fbd2ac7d52fe941afeea814fcb7a40
marceloriv <ma.riveracontreras@duocuc.com>
2025-10-06 14:57:43 -0300

    Implementar la función addToCart en el hook useUserCart para gestionar la adición de artículos al carrito

--- src/hooks/userCart.js
+++ src/hooks/userCart.js
@@ -10,5 +10,17 @@ export const useUserCart = () => {
     const MAX_ITEMS = 5
     const MIN_ITEMS = 1
 
-    function addToCart(item) { }
+    function addToCart(item) {
+        const itemExists = cart.findIndex((audifono) => audifono.id === item.id)
+        if (itemExists >= -1) {
+            if (cart[itemExists].quantity >= MAX_ITEMS) return
+            const updateCart = [...cart]
+            updateCart[itemExists].quantity++
+            setCart(updateCart)
+
+        } else {
+            item.quantity = 1
+            setCart([...cart, item])
+        }
+    }
 }
